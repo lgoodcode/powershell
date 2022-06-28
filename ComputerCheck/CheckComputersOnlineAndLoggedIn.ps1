0ab20e9e0521c856c1283fc5d01651a0bc1be36c
Good <lgood@olympic.edu>
2022-06-28 13:58:09 -0700

    output to csv file

--- ComputerCheck/CheckComputersOnlineAndLoggedIn.ps1
+++ ComputerCheck/CheckComputersOnlineAndLoggedIn.ps1
@@ -4,13 +4,17 @@ Write-Host "You can specify a room or a specific computer from any campus.`n"
 Write-Host "Initializing AD`n"
 Import-Module -Name ActiveDirectory
 
+$WRITE = 0
 $CONTINUE = 0
 $CHOICES = '&Yes', '&No'
+$DESKTOP = "$env:USERPROFILE\desktop\results.csv"
 # $ROOM_AND_COMP_REGEX = '^(id\d{5}|\d{5}[a-fA-F]?|[PpSs]\d{4})(-\d\d-\d{5})?$'
 
 while ($CONTINUE -eq 0) {
     $DOMAIN = 'instruction'
     $query = Read-Host -Prompt 'Enter the computer name (e.g., BBRRR[A]-CC-TTTTT or (P|S)BBRR-CC-TTTTT)'
+    # Will contain the results of the testing so it can be used to output to a file if desired
+    $queries = $null
 
     if (-not ($query -imatch '^(\d\d|id\d{5}|\d{5}[a-fA-F]?|[PpSs](\d|\d{4}))(-\d\d-\d{5})?$')) {
         Write-Host 'Must enter a valid room or computer name'
@@ -56,6 +60,9 @@ while ($CONTINUE -eq 0) {
 
         Write-Host 'Testing computer(s) network status...'
 
+        # For each computer, start a job testing the connection. Once each job is then, it will return 
+        # the object we will use containing the computer name, boolean value indicating if it is 
+        # online or not, and then the username, which defaults to null for no user logged in.
         $online =  $computers | ForEach-Object { Test-Connection $_ -Count 1 -AsJob } | Wait-Job | Receive-Job | ForEach-Object {[PSCustomObject]@{
             Computer = $_.Address
             Online = $(if ($_.StatusCode -eq 0) { $true } else { $false })
@@ -64,6 +71,9 @@ while ($CONTINUE -eq 0) {
 
         Write-Host 'Querying computer(s)...'
 
+        # With the results of the connection testing, if the computer is online, we will perform the
+        # query using `qwinsta` rather than PS `query user`, which for some reason would return buggy
+        # results and sometimes takes longer than `qwinsta`. 
         $queries = $online | ForEach-Object {
             if ($_.Online) {
                 # Run the command to check active session and sends the error, if any, to null
@@ -85,6 +95,7 @@ while ($CONTINUE -eq 0) {
                             } else {
                                 $_.User = $matches[0]
                             }
+                            # We found the active user line, stop looping the result
                             break
                         }
                     }
@@ -95,6 +106,38 @@ while ($CONTINUE -eq 0) {
         }
         
         Write-Output $queries | Format-Table -AutoSize
+    }
+
+    $WRITE = $host.UI.PromptForChoice(' ', 'Write output to file?', $CHOICES, 1)
+
+    if ($WRITE -eq 0) {
+        $path = Read-Host -Prompt "Enter directory to save file [desktop]"
+
+        while ($true) {
+            if ($path -eq '') {
+                $path = $DESKTOP
+                break
+            }
+
+            if (Test-Path $path -IsValid) {
+                if (-not $path.endsWith('\')) {
+                    $path = $path + '\'
+                }
+                $path = $path + "results.csv"
+                break
+            }
+
+            Write-Host 'Invalid path. Try again.'
+        }
+
+        # Convert the result object to CSV and replace the first line which contains
+        # PS information about the object converted, which we don't want, to specify
+        # the delimiter for excel to use.
+        $csv = $queries | ConvertTo-Csv
+        $csv[0] = 'sep=,'
+
+        write-output "path $path"
+        $csv | Out-File -FilePath $path
     }
 
     $CONTINUE = $host.UI.PromptForChoice(' ', 'Query another computer or room?', $CHOICES, 1)
